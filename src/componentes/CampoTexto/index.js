26508e9f8290e87885f2217a9362b5dfbd5e5a10
Walter Liendo <walter.liendo@gmail.com>
2023-08-03 20:31:47 -0300

    creacion de componente MiOrg

--- src/componentes/CampoTexto/index.js
+++ src/componentes/CampoTexto/index.js
@@ -4,7 +4,7 @@ const CampoTexto = ( props ) => {
     const placeholderModificado = `${props.placeholder}...` //// modifico mis props 
     return <div className="campo-texto">
         <label>{props.titulo}</label>
-        <input placeholder={ placeholderModificado } />
+        <input placeholder={ placeholderModificado } required={props.required} />
         </div>
 }
 
---
+++ src/componentes/MiOrg/index.js
@@ -0,0 +1,10 @@
+import "./MiOrg.css"
+
+const MiOrg = () => {
+    return <section className="orgSection">
+        <h3>Mi Organización</h3>
+        <img src="/img/add.png" alt="add" />
+    </section>
+}
+
+export default MiOrg;
