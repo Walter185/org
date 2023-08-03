aa95430c5928d284afbc06c423c2cb2aa58c48e0
Walter Liendo <walter.liendo@gmail.com>
2023-08-03 19:06:35 -0300

    creacion del boton crear

--- src/componentes/Header/Formulario/Formulario.js
+++ src/componentes/Header/Formulario/Formulario.js
@@ -1,5 +1,7 @@
 import "./Formulario.css";
 import CampoTexto from "../../CampoTexto";
+import ListaOpciones from "../../ListaOpciones";
+import Boton from "../../Boton";
 
 const Formulario = () => {
   return (
@@ -9,6 +11,10 @@ const Formulario = () => {
         <CampoTexto titulo="Nombre" placeholder="Ingresar nombre" />
         <CampoTexto titulo="Puesto" placeholder="Ingresar puesto" />
         <CampoTexto titulo="Foto" placeholder="Ingresar enlace de foto" />
+        <ListaOpciones />
+        <Boton>
+          Crear
+        </Boton>
       </form>
     </section>
   );
